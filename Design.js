fc9426c2ee64d429c96d199eb971a488d52ccd3b
Utkarsh Kumar <utkarsh9799@gmail.com>
2018-08-19 17:46:17 +0530

    Added script to create a grid dynamically via user input

---
+++ Design.js
@@ -0,0 +1,19 @@
+// Select color input
+
+
+// Selecting size input
+$('#size').on('click', function(){
+	var row = $('#row').val();
+	var column =$('#column').val();
+	makeGrid(row,column);
+});
+
+function makeGrid(r,c) {
+	for(var i = 0; i < r; i++){
+		$('#canvas').append("<tr></tr>");
+		for(var j = 0; j < c ; j++){
+			$('tr:last').append("<td></td>");
+		}
+		event.preventDefault();
+	}
+}
